6e75a91eb2109a92d51a26ffe46809630250edcb
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-07-03 09:53:21 +0200

    feat: Create files for styles

--- lib/styles/my_colors.dart
+++ lib/styles/my_colors.dart
@@ -1,4 +1,9 @@
 
 import 'package:flutter/material.dart';
 
-Color primaryColor = const Color(0xf1C15D);
+Color primaryColor = const Color(0xFFF1C15D);
+Color primaryColorHover = const Color(0xFFF1C15D).withAlpha(30);
+
+Color primaryTextColor = const Color(0xFFFFFFFF);
+Color primaryTextColorDark = const Color(0xFFEEEEEE);
+Color backgroundView = const Color(0xFF1D1D1D);
---
+++ lib/styles/my_dimens.dart
@@ -0,0 +1,1 @@
+double sizeAvatar = 36;
