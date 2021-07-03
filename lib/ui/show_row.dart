407b3fa0d4a8d2aa2a9b1808fa08bf801b3fc8fd
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-07-03 09:56:07 +0200

    feat: First version of the show row inside list

--- lib/ui/show_row.dart
+++ lib/ui/show_row.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:syntaxfm_app/models/show_model.dart';
+import 'package:syntaxfm_app/styles/my_styles.dart' as mStyles;
 
 class ShowRow extends StatelessWidget {
-
   final Show show;
 
-  const ShowRow({ Key? key, required this.show }) : super(key: key);
+  const ShowRow({Key? key, required this.show}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +13,25 @@ class ShowRow extends StatelessWidget {
       onTap: () {
         print('Tap on show n° ${show.number} with title: ${show.title}');
       },
-      child: ListTile(
-        title: Text(show.title),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8),
+        child: ListTile(
+          leading: Image.asset(
+            "assets/icons/logo.png",
+            width: 64,
+          ),
+          title: Padding(
+            padding: EdgeInsets.only(bottom: 12),
+            child: Text(
+              'Episode ${show.displayNumber}',
+              style: mStyles.textStyleSubtitle1,
+            ),
+          ),
+          subtitle: Text(
+            show.title,
+            style: mStyles.textStyleSubtitle2,
+          ),
+        ),
       ),
     );
   }
