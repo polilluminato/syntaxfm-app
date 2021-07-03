678bd3c9ee45d2d65a38ea04ea39b234ea95c001
Alberto Bonacina <bonacina.alberto@gmail.com>
2021-07-03 09:55:15 +0200

    feat: New home layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
+import 'package:avatars/avatars.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:syntaxfm_app/models/error_message_model.dart';
 import 'package:syntaxfm_app/models/show_model.dart';
 import 'package:syntaxfm_app/repository/show_repository.dart';
+import 'package:syntaxfm_app/styles/my_dimens.dart' as mDimens;
+import 'package:syntaxfm_app/styles/my_styles.dart' as mStyles;
 import 'package:syntaxfm_app/ui/show_row.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -36,37 +39,91 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Title",
-          style: GoogleFonts.notoSans(
+          "Syntax.fm Podcast",
+          style: GoogleFonts.roboto(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
         ),
       ),
-      body: RefreshIndicator(
-        onRefresh: () async {
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Text("Hosts", style: mStyles.textStyleHeadline6),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                SizedBox(
+                  width: 16,
+                ),
+                Avatar(
+                  shape: AvatarShape.circle(mDimens.sizeAvatar),
+                  useCache: true,
+                  sources: [
+                    GitHubSource('wesbos'),
+                  ],
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Avatar(
+                  shape: AvatarShape.circle(mDimens.sizeAvatar),
+                  useCache: true,
+                  sources: [
+                    GitHubSource('stolinski'),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Text("Shows", style: mStyles.textStyleHeadline6),
+            ),
+            FutureBuilder<List<Show>>(
+              future: _futureShows,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final items = snapshot.data!;
+                  return ListView.separated(
+                    separatorBuilder: (BuildContext context, int index) => Divider(),
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ShowRow(show: items[index]);
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  // Show failure error message.
+                  final failure = snapshot.error as ErrorMessage;
+                  return Center(child: Text(failure.message));
+                }
+                // Show a loading spinner.
+                return const Center(child: CircularProgressIndicator());
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
           _refreshPostList();
         },
-        child: FutureBuilder<List<Show>>(
-          future: _futureShows,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final items = snapshot.data!;
-              return ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ShowRow(show: items[index]);
-                },
-              );
-            } else if (snapshot.hasError) {
-              // Show failure error message.
-              final failure = snapshot.error as ErrorMessage;
-              return Center(child: Text(failure.message));
-            }
-            // Show a loading spinner.
-            return const Center(child: CircularProgressIndicator());
-          },
-        ),
+        child: const Icon(Icons.sync),
       ),
     );
   }
